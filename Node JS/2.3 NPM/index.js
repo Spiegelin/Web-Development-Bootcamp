208d96f6acdc6c5aacd8633973ca86d981c52a33
Diego Espejo <100853421+Spiegelin@users.noreply.github.com>
2024-01-01 20:26:08 -0600

    Complete Course

---
+++ Node JS/2.3 NPM/index.js
@@ -0,0 +1,12 @@
+//var generateName = require('sillyname');
+
+//import generateName from 'sillyname';
+import superheroes from "superheroes";
+
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+var heroName = superheroes.random();
+
+console.log(`My hero name is ${heroName}.`);
